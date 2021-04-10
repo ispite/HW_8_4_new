00a804cacc09ebe59273ad1b1d4560e2d2dec5d3
Denis Solovyov <infinityx93@gmail.com>
2021-04-10 17:29:08 +0300

    HW 8.4 add spaces and semicolons

--- app/src/main/java/ru/skillbox/codeconventions_hw8_4_new/Homework_8.4.kt
+++ app/src/main/java/ru/skillbox/codeconventions_hw8_4_new/Homework_8.4.kt
@@ -5,8 +5,8 @@ import classes.Wheel
 fun main() {
     val wheel = Wheel()
     wheel.catchCheck()
-    wheel.catchPressureException(-2.0)
-    wheel.catchPressureException(2.0)
-    wheel.catchPressureException(20.0)
-    wheel.catchCheck()
+    wheel.catchPressureException(-2.0);
+    wheel.catchPressureException(2.0);
+    wheel.catchPressureException(20.0);
+    wheel.catchCheck();
 }
